66ea8b2818be8390ea3c648a977c194199e03851
Gordon Lesti <info@gordonlesti.com>
2016-11-18 18:01:38 +0100

    reinit

--- program/src/main/java/swf/algo/DistanceCalculator.java
+++
@@ -1,5 +0,0 @@
-package swf.algo;
-
-public interface DistanceCalculator<T> {
-  public double calculateDistance(T object1, T object2);
-}
--- program/src/main/java/swf/algo/Filter.java
+++
@@ -1,7 +0,0 @@
-package swf.algo;
-
-import swf.model.TimeSeries;
-
-public interface Filter<T> {
-  public boolean isAccepted(T object);
-}
